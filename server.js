114437f6fb815981497e1ad44cafff49fb63b3a9
HenrikMarshall <henrik.marshall@gmail.com>
2025-12-07 01:23:56 -0500

    Add Firebase authentication

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 app.get('/debate', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
